a841f3b9f3fdaac274f960e65e5bee063f730873
Salman Shaikh (Eocean) <salmanzulfiqar04@gmail.com>
2026-01-14 11:35:13 +0500

    setup f.end and b.end

---
+++ day3/Backend/server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send('Server is Ready');
+// })
+
+app.get('/jokes', (req,res) => {
+    const jokes = [
+           { id: 1,
+            title: "Why don't scientists trust atoms?",
+            joke: "Why don't scientists trust atoms? Because they make up everything!" 
+          },
+         { 
+            id: 2,
+            title: "Scarecrow Award",
+            joke: "Why did the scarecrow win an award? Because he was outstanding in his field!"
+        },
+           {
+             id: 3,
+             title: "Skeleton Fight",
+             joke: "Why don't skeletons fight each other? They don't have the guts."
+         },
+         {
+            id: 4,
+            title: "Why hasn't Salman Khan married yet?",
+            joke: "Because he is still searching for his 'Dulhan'!"
+         },
+         {
+            id: 5,
+            title: "Why did the bicycle fall over?",
+            joke: "Because it was two-tired!"
+         }
+    ]
+
+    res.send(jokes);
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
