83867d4875c710f904792b3e37d146a14dbb7d44
Salman Shaikh (Eocean) <salmanzulfiqar04@gmail.com>
2026-03-25 17:57:09 +0500

    mvc

--- MVC/index.js
+++ MVC/index.js
@@ -15,3 +15,5 @@ connectDB();
 app.listen(port, () => {
     console.log(`The Server has been Started by ${admin} at Port ${port}`);
 });
+
+// this is MVC pattern we have a folder for middleware one file for connection to mongoose one folder for routes and one folder for controllers one folder for models and one folder for views
